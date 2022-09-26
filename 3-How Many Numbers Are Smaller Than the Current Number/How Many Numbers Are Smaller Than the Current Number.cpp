ea23bbe1a8aa5f171430a40b183da2c585492111
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-09-26 22:58:12 +0300

    Update How Many Numbers Are Smaller Than the Current Number.cpp

--- 3-How Many Numbers Are Smaller Than the Current Number/How Many Numbers Are Smaller Than the Current Number.cpp
+++ 3-How Many Numbers Are Smaller Than the Current Number/How Many Numbers Are Smaller Than the Current Number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         vector<int> v, v2;
         v2 = nums;
-        int counter = 0;
         sort(v2.begin(), v2.end());
-        for ( int i=0; i< v2.size(); i++) {
-            if (nums[i] == target) {
-                auto index = find(v2.begin(), v2.end(), nums[i]);
-                v.push_back((index-v2.begin()) +  counter);
-                counter ++;
-            }
+        for ( int i = 0; i<v2.size(); i++) {
+            auto index = find(v2.begin(), v2.end(), nums[i]);
+            
+            v.push_back(index-v2.begin());
         }
         return v;
     }
