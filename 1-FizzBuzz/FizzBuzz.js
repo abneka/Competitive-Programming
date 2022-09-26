fcff22eebe662d27445311001dd9b840cd888886
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-09-26 11:07:07 +0300

    Create FizzBuzz.js

---
+++ 1-FizzBuzz/FizzBuzz.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let res = []
+    
+    for (let i=1; i<=n; i++) {
+        if (i % 5 === 0 && i%3 === 0) res.push("FizzBuzz")
+        else if (i%3 === 0) res.push("Fizz")
+        else if (i%5 === 0) res.push("Buzz")
+        else res.push(`${i}`)
+    }
+    
+    return res
+};
