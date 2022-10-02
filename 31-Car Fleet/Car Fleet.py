ddb5ff578efcaf887cdc5311e7a831525db735d1
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-10-03 00:20:22 +0300

    Create Car Fleet.py

---
+++ 31-Car Fleet/Car Fleet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p,s] for p, s in zip(position, speed)]
+        
+        stack = []
+        for p, s in sorted(pair)[::-1]:
+            time = (target-p)/s
+            if stack and stack[-1] >= time:
+                continue
+            stack.append(time)
+        return len(stack)
