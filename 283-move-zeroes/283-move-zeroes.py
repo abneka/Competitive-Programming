3bea93f94a0594da7d798a4e8227125ce13e21f7
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-10-09 15:21:21 +0300

    Time: 351 ms (49.46%), Space: 15.5 MB (96.83%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, 0
+        
+        while r < len(nums):
+            if nums[r] != 0 :
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
+            r += 1
