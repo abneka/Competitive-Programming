dee847423d9679e4539eadc0036bca9d7f73d915
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-12-24 12:17:31 +0300

    Time: 42 ms (83.25%), Space: 13.9 MB (83.64%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -3,7 +3,4 @@ class Solution:
         order = {char : index for index, char in enumerate(order)}
         
         words = [[order[c] for c in w] for w in words]
-        # print(order)
-        # words.sort()
-        # print(words)
         return all(words[i] <= words[i+1] for i in range(len(words) - 1))
