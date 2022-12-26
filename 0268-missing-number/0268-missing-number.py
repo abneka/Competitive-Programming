ab674bdf4ecd24cde28fab0b63a05bda6a749e88
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2022-12-26 19:54:05 +0300

    Time: 139 ms (89.86%), Space: 15.3 MB (43.40%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums) +1
-        total = sum(num for num in range(length))
-        return total - sum(nums)
+        length = len(nums) +1 #length of number with the missing number
+        total = sum(num for num in range(length)) #sum of numbers with missing number
+        return total - sum(nums) #difference between sum of expected and original value
