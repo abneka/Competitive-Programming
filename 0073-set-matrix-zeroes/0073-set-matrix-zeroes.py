ac26b8848f612c6a6542d1c1aecbf591abb53b6f
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-01-26 17:38:02 +0300

    Time: 142 ms (51.59%), Space: 14.7 MB (95.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,17 +4,25 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        R = len(matrix)
-        C = len(matrix[0])
-        rows, cols = [], []
+        row_length = len(matrix)
+        col_length = len(matrix[0])
+        
+        def default():
+            return 1
+        
+        rows = defaultdict(default)
+        cols = defaultdict(default)
+        # row['9'] = 0
+        # print(row['9'])
+        # rows, cols = [], []
 
-        for i in range(R):
-            for j in range(C):
+        for i in range(row_length):
+            for j in range(col_length):
                 if matrix[i][j] == 0:
-                    rows.append(i)
-                    cols.append(j)
+                    rows[str(i)] = 0
+                    cols[str(j)] = 0
 
-        for i in range(R):
-            for j in range(C):
-                if i in rows or j in cols:
+        for i in range(row_length):
+            for j in range(col_length):
+                if not rows[str(i)] or not cols[str(j)]:
                     matrix[i][j] = 0
