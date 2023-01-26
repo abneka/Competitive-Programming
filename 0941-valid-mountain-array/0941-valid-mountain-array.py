7058dd561eada4ae2ad2644345b9df0e286f8c40
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-01-26 23:07:39 +0300

    Time: 244 ms (36.24%), Space: 15.4 MB (63.72%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        
+        length = len(arr)
+        
+        if length <3:
+            return False
+        
+        maxi = arr[0]
+        flag = True
+        dec_flag = True
+        
+        for index in range(1, length):
+            
+            if arr[index] > maxi and flag:
+                maxi = arr[index]
+                dec_flag = False
+                if index == length -1:
+                    return False
+                continue
+                
+            elif arr[index] < maxi:
+                flag = False
+                maxi = arr[index]
+                if dec_flag:
+                    return False
+                continue
+            
+            
+            
+            return False
+        
+        return True
