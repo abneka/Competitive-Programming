05ea9ea82f287de280944becd09292b30daa45a6
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-01-26 19:54:28 +0300

    Time: 53 ms (31.07%), Space: 13.9 MB (91.53%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,34 +1,30 @@
 class Solution:
-    def numMagicSquaresInside(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        cnt = 0
-        # Construct the 3x3 square
-        for i in range(len(grid)-2):
-            for j in range(len(grid[0])-2):
-                temp_grid = [grid[i+k][j:j+3] for k in range(3)]
-                print(temp_grid, i, j, len(grid))
-                if self.isMagicSquare(temp_grid):
-                    cnt += 1
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
         
-        return cnt
+        counter = 0
+        
+        for row in range(len(grid)-2):
+            for col in range(len(grid[0])-2):
+                temp_grid = [grid[row+k][col:col+3] for k in range(3)]
+                print(temp_grid, row, col, len(grid))
+                
+                check_nums = [num for row in temp_grid for num in row]
+                if sorted(check_nums) != [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                    continue
+
+                row_sums = [sum(row) for row in temp_grid]
+                col_sums = [sum([row[i] for row in temp_grid]) for i in range(3)]
+                diag_sums = [sum([temp_grid[i][i] for i in range(3)]), (temp_grid[0][2] + temp_grid[1][1] + temp_grid[2][0])]
+                
+                row_sums.extend(col_sums)
+                row_sums.extend(diag_sums)
+                
+                
+                if len(set(row_sums)) == 1:
+                    counter += 1
+        
+        return counter
         
     
-    def isMagicSquare(self, grid):
-        '''
-        Check whether the given grid is a magic square
-        '''
-        # Check the elements
-        flat = [num for row in grid for num in row]
-        if sorted(flat) != [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-            return False
+
         
-        # Check the row, column and diagnal sums
-        row_sums = [sum(row) for row in grid]
-        col_sums = [sum([row[i] for row in grid]) for i in range(3)]
-        diag_sums = [sum([grid[i][i] for i in range(3)]), (grid[0][2] + grid[1][1] + grid[2][0])]
-        row_sums.extend(col_sums)
-        row_sums.extend(diag_sums)
-        return len(set(row_sums)) == 1
