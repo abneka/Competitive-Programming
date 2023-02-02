52b05c89f8681fc47abe7fc9e8a011c058d0b643
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-02-02 17:30:05 +0300

    Time: 3178 ms (5.09%), Space: 14.2 MB (17.59%) - LeetHub

---
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -0,0 +1,67 @@
+class Solution:
+    def largestMerge(self, word1: str, word2: str) -> str:
+        length1 =len(word1)
+        length2 = len(word2)
+        length = length1 + length2
+        
+        char1 = 0
+        char2 = 0
+        
+        merge = ''
+        cache = ''
+        count = 0
+        is_equal = False
+        
+        while char1 < length1 and char2 < length2:
+            
+            
+            
+            if word1[char1] > word2[char2]:
+                merge += word1[char1]
+                char1 += 1
+            
+            elif word1[char1] < word2[char2]:
+                merge += word2[char2]
+                char2 += 1
+            
+            else:
+                if is_equal:
+                    merge += word1[char1]
+                    is_equal = False
+                    char1 += 1
+                    continue
+                
+                if word1[char1:] == word2[char2:]:
+                    is_equal = True
+                    continue
+                
+                count = 0
+                while word1[char1: char1 + count] == word2[char2: char2 + count]:
+                    count += 1
+                    if char1 + count == length1 or char2 + count == length2:
+                        break
+                        
+                if word1[char1: char1 + count] > word2[char2: char2 + count]:
+                    merge += word1[char1]
+                    char1 += 1
+                    
+                elif word1[char1: char1 + count] < word2[char2: char2 + count]:
+                    merge += word2[char2]
+                    char2 += 1
+                
+                else:
+                    if char1 + count == length1:
+                        merge += word2[char2]
+                        char2 += 1
+                    elif char2 + count == length2:
+                        merge += word1[char1]
+                        char1 += 1
+                    
+        if char2 == length2:
+            merge += word1[char1:]
+            
+        if char1 == length1:
+            merge += word2[char2:]
+        
+        
+        return merge
