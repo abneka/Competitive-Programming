7cb8699aba6da5cee5b5335bf6d38640574991dc
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-02-24 19:06:43 +0300

    Time: 1104 ms (19.00%), Space: 20 MB (84.24%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -2,14 +2,21 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         left = 0
         basket = Counter()
+        length = 0
         
         ans = 0
         
         for index, fruit in enumerate(fruits):
+            
+            if not basket[fruit]:
+                length += 1
+            
             basket[fruit] += 1
-            while len(basket) > 2:
+            
+            while length > 2:
                 basket[fruits[left]] -= 1
                 if not basket[fruits[left]]:
+                    length -= 1
                     del basket[fruits[left]]
                 left += 1
             
