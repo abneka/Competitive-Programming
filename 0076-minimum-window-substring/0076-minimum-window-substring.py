8ba405e1524b8df40267e36f1f67710f5b1256be
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-02-27 15:56:14 +0300

    Time: 1558 ms (5.00%), Space: 14.7 MB (76.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -6,21 +6,21 @@ class Solution:
         length = len(s)
         
         left = 0
-        ans = [length, [0,0]]
+        ans = [length, '']
+        string = ''
         
         for index, char in enumerate(s):
+            string += char
             
             if t[char]:
                 count[char] += 1
             
             while t <= count:
-                ans = min(ans, [(index - left), [left, index + 1]])
+                ans = min(ans, [(index - left), string[left:]])
                 
                 if t[s[left]]:
                     count[s[left]] -= 1
                 
                 left += 1
         
-        left, right = ans[1]
-        
-        return s[left : right]
+        return ans[1]
