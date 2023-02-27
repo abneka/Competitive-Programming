88333e0ba024c4aaef2a77f229366840ad79e494
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-02-27 15:06:07 +0300

    Update 0560-subarray-sum-equals-k.py

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        pSum = {0: 1}
-        total= res = 0
+        prefix_sum = Counter()
+        prefix_sum[0] = 1
         
-        for i in nums:
-            total += i
-            if total - k in pSum:
-                res += pSum[total-k]
-            
-            if total in pSum:
-                pSum[total] += 1   
-                continue
-            
-            pSum[total] = 1
+        total = 0
+        count = 0
         
-        return res
+        for num in nums:
+            total += num
+            if prefix_sum[total - k]:
+                count += prefix_sum[total-k]
+            
+            prefix_sum[total] += 1
+        
+        return count
