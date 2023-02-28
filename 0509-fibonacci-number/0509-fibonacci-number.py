5eb9b247981f031711f67ee4a564addf7fe0646d
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-02-28 17:29:18 +0300

    Time: 2661 ms (5.00%), Space: 13.7 MB (93.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,17 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return (self.fib(n-1) + self.fib(n-2))
+        
+        memo = {}
+        
+        def fibo(num):
+            if num == 0:
+                return 0
+            elif num == 1:
+                return 1
+            if num in memo:
+                return memo[num]
+            
+            memo[num] = (self.fib(num-1) + self.fib(num-2))
+            return memo[num]
+
+        return fibo(n)
