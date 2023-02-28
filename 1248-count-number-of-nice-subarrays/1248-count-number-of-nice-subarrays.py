2d29c81c56ad4c29433ab81fdfd6a08730195a96
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-02-28 14:55:04 +0300

    Update 1248-count-number-of-nice-subarrays.py

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,18 +1,22 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        nums = [num%2 for num in nums]
-        prefix = {0:1}
+        prefix_sum = Counter()
+        prefix_sum[0] = 1
+        
         total = 0
-        res = 0
-        for i in nums:
-            total += i
-            if total-k in prefix:
-               res += prefix[total-k]
+        count = 0
+        
+        for num in nums:
+            if not num % 2:
+                num = 0
+            else:
+                num = 1
+            total += num
             
-            if total in prefix:
-                prefix[total] += 1
-                continue
+            if prefix_sum[total - k]:
+                count += prefix_sum[total-k]
             
-            prefix[total] = 1
-            
-        return res
+            prefix_sum[total] += 1
+        
+        
+        return count
