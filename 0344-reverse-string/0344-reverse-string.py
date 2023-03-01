3c8e2c7fe95733bbf95937c64b3b9d60fbb91e15
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-03-01 17:48:52 +0300

    Update 0344-reverse-string.py

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,8 +4,20 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
-        last_index = len(s) - 1
+        l = 0
+        r = len(s) - 1
         
-        for index, char in enumerate(s[:]):
-            s[last_index - index] = char
+        def reverse(left, right):
+            if left >= right:
+                return 
+            
+            s[left], s[right] = s[right], s[left]
+            reverse(left + 1, right - 1)
+        
+        reverse(l, r)
+        
+#         last_index = len(s) - 1
+        
+#         for index, char in enumerate(s[:]):
+#             s[last_index - index] = char
             
