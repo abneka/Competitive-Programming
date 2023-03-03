a1895ef91be69a8352a3b6793f1166b789706e36
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-03-03 15:20:28 +0300

    Time: 702 ms (28.91%), Space: 19.8 MB (6.98%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,33 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        left = 1
+        right = sum(weights)
+        res = right
+        
+        while left <= right:
+            capacity = (left + right) // 2
+            
+            total = 0
+            count = 0
+            
+            for weight in weights:
+                if weight > capacity:
+                    count = days + 1
+                    break
+                    
+                if total + weight > capacity:
+                    count += 1
+                    total = 0
+                total += weight
+            
+            if total:
+                count += 1
+            
+            if count <= days:
+                res = min(res, capacity)
+                right = capacity - 1
+            
+            else:
+                left = capacity + 1
+        
+        return res
