e5e35bfaa251d9b8e5ce048660bb530a21d72315
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-03-06 16:02:31 +0300

    Time: 116 ms (48.40%), Space: 14.7 MB (50.18%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -6,33 +6,33 @@ class Solution:
         words.sort()
         length = len(words)
         
-        def searchInsert():
-            low = 0
-            high = len(words) - 1
-            mid = 0
-
-            while low <= high:
-                mid = (low + high) // 2
-
-                if words[mid] < size:
-                    low = mid + 1
-
-                elif words[mid] > size:
-                    high = mid - 1
-
-                else:
-                    low = mid + 1
-                    mid += 1
-
-            if mid < len(words) and words[mid] < size:
-                return mid + 1
-
-            return mid
-        
         for index, size in enumerate(queries):
-            queries[index] = length - searchInsert()
+            queries[index] = length - self.searchInsert(words, size)
                 
         return queries
+    
+    def searchInsert(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if nums[mid] < target:
+                low = mid + 1
+                
+            elif nums[mid] > target:
+                high = mid - 1
+                
+            else:
+                low = mid + 1
+                mid += 1
+        
+        if mid < len(nums) and nums[mid] < target:
+            return mid + 1
+        
+        return mid
     
     def freq(self, words):
         ans = []
