31f275e19782788c37cdc0bfe5fd9c5a322665a8
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-03-10 16:56:46 +0300

    Time: 31 ms (88.16%), Space: 13.8 MB (97.04%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -1,32 +1,19 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-        # arr = []
         def splitNcheck(string, prev):
             if not string:
-                # print(prev)
                 return True
             
-            
             flag = False
-            # print(string, prev)
             for index in range(len(string)):
                 num = int(string[:index + 1])
                 
-                if prev ==  -1:
-                    # print(num)
-                    if int(string) == num:
-                        break
+                if prev ==  -1 and int(string) != num:
                     flag = flag or splitNcheck(string[index + 1:], num)
-                    # prev = num + 1
                 
-                if num != prev - 1:
-                    # print(num, prev)
-                    continue
-                
-                # prev = num
-                flag = flag or splitNcheck(string[index + 1:], num)
+                elif num == prev - 1:
+                    flag = flag or splitNcheck(string[index + 1:], num)
+                    
             return flag
         
         return splitNcheck(s, -1)
-            
-        # return False
