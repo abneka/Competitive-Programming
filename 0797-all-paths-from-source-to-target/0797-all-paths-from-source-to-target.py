d0bd7f2da5083fd2cfd2eb51d1fe680933ebf6a2
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-04-26 14:35:50 +0300

    Time: 121 ms (5.21%), Space: 18.6 MB (5.06%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,32 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        graphs = defaultdict(list)
+        maximum = 0
+        for index, nodes in enumerate(graph):
+            graphs[index] = nodes
+            if nodes:
+                maximum = max(max(nodes), maximum)
+        
+        ans = []
+        def dfs(path, index):
+            if not graphs[index]:
+                if index == maximum:
+                    path.append(index)
+                    ans.append(path[::])
+                    path.pop()
+                return
+            
+            if index == maximum:
+                path.append(index)
+                ans.append(path[::])
+                path.pop()
+                return
+            
+            path.append(index)
+            
+            for ind in graphs[index]:
+                dfs(path, ind)
+            path.pop()
+        
+        dfs([], 0)
+        return ans
