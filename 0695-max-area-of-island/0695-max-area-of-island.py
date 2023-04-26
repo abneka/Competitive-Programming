b869c6947d2425a39a61f314d22402295ec4652b
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-04-26 09:18:54 +0300

    Time: 158 ms (25.05%), Space: 20.4 MB (5.29%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,30 +4,29 @@ class Solution:
         max_col = len(grid[0])
         moves = [(0,1),(0,-1),(1,0),(-1,0)]
         
-        def dfs(row, col, visited):
+        visited = set()
+        
+        def dfs(row, col, depth):
             if (row, col) in visited:
-                return visited
+                return 0
+            
             if row == max_row or col == max_col or row < 0 or col < 0 or not grid[row][col]:
-                return visited
+                return 0
             
             visited.add((row, col))
-            
+            total = 1
             for move in moves:
                 move_r, move_c = move
-                visited.update(dfs(row + move_r, col + move_c, visited))
+                total += dfs(row + move_r, col + move_c, depth + 1)
             
-            return visited
+            return total
         
         
-        visited = set()
         maxi = 0
         for r, row in enumerate(grid):
             for c, col in enumerate(row):
                 if not col or (r,c) in visited:
                     continue
-                
-                new = dfs(r,c,set())
-                maxi = max(maxi, len(new))
-                visited.update(new)
+                maxi = max(dfs(r, c, 1), maxi)
         
         return maxi
