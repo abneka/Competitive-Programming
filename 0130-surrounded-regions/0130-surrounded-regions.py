fbe0a6cb435215af3dbd2f8b8dc9a38c940f8647
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-04-28 19:53:07 +0300

    Time: 166 ms (19.39%), Space: 18.3 MB (16.77%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,39 +8,28 @@ class Solution:
         max_row = len(board)
         max_col = len(board[0])
         start = []
+        inbound = lambda row, col : 0 <= row < max_row and 0 <= col < max_col
         
-        for row_index, row in enumerate(board):
-            if row[0] == 'O':
-                start.append((row_index, 0))
-                
-            if max_col > 1:
-                if row[max_col - 1] == 'O':
-                    start.append((row_index, max_col - 1))
-                    
-            if row_index == 0 or row_index == max_row - 1:
-                for col in range(1, max_col - 1):
-                    if row[col] == 'O':
-                        start.append((row_index, col))
-                    
         not_surrounded = set()
         def dfs(row, col):
-            if row == max_row or col == max_col or col < 0 or row < 0:
-                return
-            
             if board[row][col] == 'X' or (row, col) in not_surrounded:
                 return
             
             not_surrounded.add((row, col))
-            for move in moves:
-                next_row, next_col = move
-                next_row += row
-                next_col += col
-                dfs(next_row, next_col)
+            
+            for next_row, next_col in moves:
+                if inbound(next_row + row, next_col + col):
+                    dfs(next_row + row, next_col + col)
         
-        for row, col in start:
-            dfs(row, col)
+        for row in range(max_row):
+            dfs(row, 0)
+            dfs(row, max_col - 1)
+        
+        for col in range(max_col):
+            dfs(0, col)
+            dfs(max_row - 1, col)
         
         for row_index, row in enumerate(board):
             for col_index, col in enumerate(row):
-                if col == 'O' and (row_index, col_index) not in not_surrounded:
+                if (row_index, col_index) not in not_surrounded:
                     board[row_index][col_index] = 'X'
