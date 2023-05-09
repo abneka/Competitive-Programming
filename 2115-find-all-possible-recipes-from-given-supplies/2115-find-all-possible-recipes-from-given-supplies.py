c6f49d146d6cc11d5ab451fcac7d3f233e1b7789
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-05-09 19:38:23 +0300

    Time: 1011 ms (30.40%), Space: 19.2 MB (24.36%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        graph = defaultdict(list)
+        dep_graph = defaultdict(int)
+        
+        for recipe, ingredient in zip(recipes, ingredients):
+            for ingrs in ingredient:
+                dep_graph[recipe] += 1
+                graph[ingrs].append(recipe)
+        
+        visited = set()
+        queue = deque()
+        
+        for supp in supplies:
+            if not dep_graph[supp]:
+                queue.append(supp)
+        
+        recipes = set(recipes)
+        
+        ans = []
+        
+        while queue:
+            node = queue.popleft()
+            if node in recipes:
+                ans.append(node)
+            visited.add(node)
+
+            for ingrs in graph[node]:
+                dep_graph[ingrs] -= 1
+                if ingrs not in visited and not dep_graph[ingrs]:
+                    queue.append(ingrs)
+        
+        return ans
+            
