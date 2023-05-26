448799b35fe2577c7e66007d1d00861cf3a0a86d
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-05-26 19:16:41 +0300

    Time: 45 ms (34.62%), Space: 16.4 MB (13.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = [-1 for _ in range(len(nums))]
+        memo = {}
         
         def climb (n):
             
@@ -9,7 +9,7 @@ class Solution:
             if n <= 1:
                 return nums[n]
             
-            if memo[n] == -1:
+            if n not in memo:
                 memo[n] = max(climb(n-2) + nums[n], climb(n-3) + nums[n])
             
             return memo[n]
