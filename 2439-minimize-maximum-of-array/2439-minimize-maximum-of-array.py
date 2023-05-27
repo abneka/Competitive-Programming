59c5f6c1799912c208421914ab7d2b38cd90b3c2
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-05-27 22:18:56 +0300

    Time: 1570 ms (8.10%), Space: 29.2 MB (5.10%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -7,9 +7,36 @@ class Solution:
          3  10 11 17
          3   5  4  5
         '''
-        total = 0
-        ans = float('-inf')
-        for index, num in enumerate(nums, 1):
-            total += num
-            ans = max(ans, ceil(total/index))
+        length = len(nums)
+        total = sum(nums)
+        maxi = max(nums)
+        left = total // length
+        right = maxi
+        ans = float('inf')
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            move = 0
+            for index in range(length - 1, -1, -1):
+                if mid >= nums[index] + move:
+                    move = 0
+                    continue
+                
+                move = (nums[index] + move) - mid
+            if not move:
+                right = mid -1
+                ans = min(ans, mid)
+                continue
+            left = mid + 1
         return ans
+            
+            
+        
+        # total = 0
+        # ans = float('-inf')
+        # for index, num in enumerate(nums, 1):
+        #     total += num
+        #     ans = max(ans, ceil(total/index))
+        #     print(ans)
+        # return ans
