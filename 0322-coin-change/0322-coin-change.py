225213017b9d40bc9391d60e211d2466678dc494
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-05-27 22:53:40 +0300

    Time: 3159 ms (5.01%), Space: 61.8 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,24 +3,22 @@ class Solution:
         '''
         
         '''
-        memo = {}
         length = len(coins)
+        memo = [[-1] * amount for _ in range(length)]
         def dp(index, val):
             if index == length or val > amount:
                 return float('inf')
             if val == amount:
                 return 0
             
-            state = (index, val)
-            
-            if not state in memo:
+            if memo[index][val] == -1:
                 take_n_repeat = dp(index, val + coins[index]) + 1
                 take_n_leave = dp(index + 1, val + coins[index]) + 1
                 leave = dp(index + 1, val)
 
-                memo[state] = min([take_n_leave, take_n_repeat, leave])
+                memo[index][val] = min([take_n_leave, take_n_repeat, leave])
                 
-            return memo[state]
+            return memo[index][val]
             
             
         ans = dp(0, 0)
