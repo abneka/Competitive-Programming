9e19c69b74fa0a8669dfcac20dce5e4616f4101b
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-06-01 10:12:08 +0300

    Time: 44 ms (42.94%), Space: 16.2 MB (39.98%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -1,21 +1,19 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        memo = [-1] * (n + 2)
+        arr = [0] * (n + 1)
         if not n:
             return 0
+        arr[1] = 1
+        maxi = 1
         
-        def getMax(n) :
-            if n <= 1:
-                return n
-            if memo[n] == -1:
-                half = n // 2
-                if n % 2:
-                    memo[n] = max(memo[n], getMax(half) + getMax(half + 1))
-                else:
-                    memo[n] = max(memo[n], getMax(half))
-            return memo[n]
-        
-        ans = 0
-        for i in range(n):
-            ans = max(ans, getMax(n - i))
-        return ans
+        for i in range(2, n + 1):
+            half = i // 2
+            new = 0
+            if i % 2:
+                new = arr[half] + arr[half + 1]
+            else:
+                new = arr[half]
+            arr[i] = new
+            maxi = max(maxi, new)
+            
+        return maxi
