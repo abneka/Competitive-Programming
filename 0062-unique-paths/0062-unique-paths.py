a0f9b8a595ed6a8281e6da77db7f56c1b5fcfe5d
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-06-01 19:40:55 +0300

    Time: 58 ms (6.54%), Space: 16.4 MB (22.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = [[-1] * n for _ in range(m)]
-        inbound = lambda row, col: row <= m and col <= n
-        def path(row, col):
-            if not inbound(row, col):
-                return 0
-            
-            if row == m and col == n:
-                return 1
-            
-            if memo[row - 1][col - 1] == -1:
-                memo[row - 1][col - 1] = path(row + 1, col) + path(row, col + 1)
-            
-            return memo[row - 1][col - 1]
+        arr = [1] * n
+        checkCol = lambda col: 0 <= col < n
         
-        return path(1, 1)
+        for row in range(1, m):
+            for col in range(n):
+                if checkCol(col - 1):
+                    arr[col] += arr[col - 1]
+        
+        return arr[-1]
