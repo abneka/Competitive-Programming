c82aeae09d2d223375b9ffa81ee9f3c73bd2d9eb
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-06-07 17:25:48 +0300

    Time: 262 ms (74.20%), Space: 20.8 MB (55.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,22 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         length = len(nums)
-        memo = {}
+        memo = [[-1] * 1001 for _ in range(length)]
         def dp(index, val):
             if index == length:
                 if val == target:
                     return 1
                 return 0
             
-            state = (index, val)
-            
-            if state not in memo:
+            if memo[index][val] == -1:
                 positive = dp(index + 1, val + nums[index])
                 negative = dp(index + 1, val - nums[index])
 
-                memo[state] = positive + negative
+                memo[index][val] = positive + negative
                 
-            return memo[state]
+            return memo[index][val]
             
             
         return dp(0, 0)
