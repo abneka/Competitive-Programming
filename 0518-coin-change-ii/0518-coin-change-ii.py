b7a3c314a6ad15281c5db88c8a7ee1c235788248
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-06-07 15:58:22 +0300

    Time: 410 ms (46.29%), Space: 16.6 MB (60.54%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,17 +1,11 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        length = len(coins)
-        memo = [[-1] * amount for _ in range(length)]
-        def dp(index, val):
-            if val == amount:
-                return 1
-            
-            if index == length or val > amount:
-                return 0
-            
-            if memo[index][val] == -1:
-                memo[index][val] = dp(index, val + coins[index]) + dp(index + 1, val)
-            
-            return memo[index][val]
+        memo = [1] + ([0] * amount)
+        inbound = lambda col : 0 <= col < amount + 1
         
-        return dp(0, 0)
+        for coin in coins:
+            for col in range(1, amount + 1):
+                if inbound(col - coin):
+                    memo[col] += memo[col - coin]
+        
+        return memo[-1]
