841dcea092a6afab52be6235193aa00d9c13a9a3
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-06 09:50:10 +0300

    Time: 405 ms (85.04%), Space: 18.8 MB (33.56%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -2,27 +2,27 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
         
-        for u, v, w in times:
-            graph[u].append((w, v))
+        for v, u, w in times:
+            graph[v].append((w, u))
         
         queue = [(0, k)]
-        distance = [float('inf')] * n
-        distance[k - 1] = 0
+        distance = [float('inf')] * (n + 1)
+        distance[k] = 0
+        distance[0] = 0
         visited = set()
         
         while queue:
-            curr, node = heappop(queue)
-            
+            currW, node = heappop(queue)
             if node in visited:
                 continue
             
             visited.add(node)
             
             for weight, neigh in graph[node]:
-                dist = curr + weight
+                dist = currW + weight
                 
-                if dist < distance[neigh - 1]:
-                    distance[neigh - 1] = dist
+                if distance[neigh] > dist:
+                    distance[neigh] = dist
                     heappush(queue, (dist, neigh))
-                    
+        
         return -1 if any(d == float('inf') for d in distance) else max(distance)
