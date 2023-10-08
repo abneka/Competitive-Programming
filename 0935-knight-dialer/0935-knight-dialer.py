04c28ed0d39e1a60d65a29e4e1f055ba30549225
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-08 22:27:35 +0300

    Time: 1895 ms (33.03%), Space: 109.2 MB (5.12%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -3,21 +3,20 @@ class Solution:
         direction = {0: (4, 6), 1: (8, 6), 2: (7, 9), 3: (4, 8),
                      4: (0, 3, 9), 5: (), 6: (1, 7, 0),
                      7: (2, 6), 8: (1, 3), 9: (2, 4)}
-        memo = {}
+        memo = [[-1] * 10 for _ in range(n)]
+        
         def dp(key, size):
             if size == 1:
                 return 1
             
-            state = (key, size)
-            
-            if state not in memo:
+            if memo[size - 1][key - 1] == -1:
                 option = 0
                 for next_key in direction[key]:
                     option += dp(next_key, size - 1)
 
-                memo[state] = option
+                memo[size - 1][key - 1] = option
             
-            return memo[state]
+            return memo[size - 1][key - 1]
         
         ans = 0
         modulo = (10 ** 9) + 7
