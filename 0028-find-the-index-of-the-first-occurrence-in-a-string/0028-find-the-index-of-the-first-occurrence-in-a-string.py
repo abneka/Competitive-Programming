a372f51cdda24e6db27fdd4d22eb5962a4258fe4
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-10 15:56:06 +0300

    Time: 36 ms (73.44%), Space: 16.2 MB (81.66%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,6 +1,31 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        result = re.search(needle,haystack)
-        if result is None:
+        nee = 0
+        curr = 0
+        modulo = (10 ** 9) + 7
+        
+        if len(needle) > len(haystack):
             return -1
-        return result.start()
+        
+        def add(num, char):
+            num *= 27
+            num += (ord(char) - ord('a') + 1)
+            num %= modulo
+            return num
+        
+        def remove(num, char):
+            num -= ((ord(char) - ord('a') + 1) * (27 ** (len(needle) - 1)))
+            return num
+        
+        for index in range(len(needle)):
+            nee = add(nee, needle[index])
+            curr = add(curr, haystack[index])
+                
+        for index in range(len(haystack) - len(needle)):
+            if curr == nee:
+                return index
+            
+            curr = remove(curr, haystack[index])
+            curr = add(curr, haystack[index + len(needle)])
+        
+        return -1 if curr != nee else len(haystack) - (len(needle))
