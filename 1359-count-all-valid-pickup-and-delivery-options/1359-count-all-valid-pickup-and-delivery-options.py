a5a3ad65b9baf404f42e35c674031adbf4a57af6
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-12 19:48:55 +0300

    Time: 42 ms (37.22%), Space: 16.3 MB (67.42%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -3,9 +3,13 @@ class Solution:
         memo = 1
         mod = (10 ** 9) + 7
         
+        def multiply(a, b, m):
+            return ((a % m) * (b % m)) % m
+        
         for num in range(1, n + 1):
             num *= 2
-            memo *= (num * (num - 1)//2)
-            memo %= mod
+            num = multiply(num, num -1, mod)
+            num //= 2
+            memo = multiply(memo, num, mod)
         
         return memo
