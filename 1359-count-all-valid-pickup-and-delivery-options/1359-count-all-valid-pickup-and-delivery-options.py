88d21221e9503e523d9d814a7387a68f92c31026
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-12 19:38:18 +0300

    Time: 44 ms (27.68%), Space: 18.1 MB (11.25%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -8,7 +8,7 @@ class Solution:
                 return 1
             num = 2 * n
             if memo[n] == -1:
-                memo[n] = dp(n - 1) * ((num * (num -1))//2) % mod
+                memo[n] = dp(n - 1) * ((num * (num -1))//2)
             return memo[n]
         
-        return dp(n)
+        return dp(n) % mod
