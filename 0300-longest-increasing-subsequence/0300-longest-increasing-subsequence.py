3f448b3f29dcc42bd99d483f7b363dbd11bcefc4
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-17 16:04:25 +0300

    Time: 3152 ms (8.59%), Space: 16.5 MB (77.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,13 +1,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        stack = [float('inf')]
+        length = len(nums)
+        memo = [1] * length
+        ans = 1
         
-        for num in nums:
-            index = bisect_left(stack, num)
-            
-            if index == len(stack):
-                stack.append(num)
-            else:
-                stack[index] = num
+        for ind, num in enumerate(nums):
+            for index in range(ind, -1, -1):
+                if nums[index] < num:
+                    memo[ind] = max(memo[index] + 1, memo[ind])
+                    ans = max(ans, memo[ind])
         
-        return len(stack)
+        return ans
+        
