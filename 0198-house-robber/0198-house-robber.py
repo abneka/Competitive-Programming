bd2a253a9f8b3b98cea574abf1de53f49ba20c3c
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-17 10:59:12 +0300

    Time: 46 ms (13.17%), Space: 16.2 MB (54.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = [0] * 2
-        memo.extend([num for num in nums])
+        memo = [0] + [num for num in nums]
         
-        for index in range(3, len(nums) + 2):
+        for index in range(3, len(nums) + 1):
             memo[index] = max(memo[index - 2] + memo[index], memo[index - 3] + memo[index])
         
         return max(memo[-1], memo[-2])
