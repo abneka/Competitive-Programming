029f3e2601cc78c1d648994b126e3dfd25ef1639
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-17 10:50:30 +0300

    Time: 30 ms (96.44%), Space: 16.1 MB (82.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @cache
-        def climb (n):
+        memo = [0] * 2
+        memo.extend([num for num in nums])
+        
+        for index in range(3, len(nums) + 2):
+            memo[index] = max(memo[index - 2] + memo[index], memo[index - 3] + memo[index])
+        
+        return max(memo[-1], memo[-2])
+        
+#         @cache
+#         def climb (n):
             
-            if n < 0:
-                return 0
-            if n <= 1:
-                return nums[n]
+#             if n < 0:
+#                 return 0
+#             if n <= 1:
+#                 return nums[n]
             
-            return max(climb(n-2) + nums[n], climb(n-3) + nums[n])
+#             return max(climb(n-2) + nums[n], climb(n-3) + nums[n])
             
         
         return max(climb(len(nums) - 1), climb(len(nums) - 2))
