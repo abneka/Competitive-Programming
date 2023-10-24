6a676f869868b3d093a079bef2cf4460fc4d2553
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-24 20:52:51 +0300

    Time: 48 ms (66.79%), Space: 16.1 MB (99.52%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort(reverse = True)
-        length = len(satisfaction)
         prefix = 0
         ans = 0
+        
         for num in satisfaction:
             prefix += num
             if prefix < 0:
