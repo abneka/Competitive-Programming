b80b25e876cc4b02ac368282e96a678696a97fce
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-10-30 15:53:13 +0300

    Added solution for Longest Increasing Subsequence

---
+++ Leetcode_problems/After-Camp-II/longest-increasing-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        stack = [float('inf')]
+
+        for num in nums:
+            index = bisect_left(stack, num)
+
+            if index == len(stack):
+                stack.append(num)
+            
+            else:
+                stack[index] = num
+        
+        return len(stack)
