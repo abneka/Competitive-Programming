72958efcca5613a1fd9e0a3f924abb6bf6663792
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-11-19 20:06:58 +0300

    Added solution for Integer to English Words

---
+++ Leetcode_problems/After-Camp-II/integer-to-english-words.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        if not num:
+            return "Zero"
+        
+        ones = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        teens = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", 
+                "Eighteen", "Nineteen"]
+        suffixes = ["", "Thousand", "Million", "Billion"]
+        
+        words = []
+        i = 0
+        while num:
+            triplet = num % 1000
+            num = num // 1000
+            
+            if not triplet:
+                i += 1
+                continue
+            
+            temp = []
+            if triplet // 100:
+                temp.append(ones[triplet // 100] + " Hundred")
+            if triplet % 100 >= 10 and triplet % 100 <= 19:
+                temp.append(teens[triplet % 10])
+            else:
+                if triplet % 100 >= 20:
+                    temp.append(tens[triplet % 100 // 10])
+                if triplet % 10:
+                    temp.append(ones[triplet % 10])
+            if i:
+                temp.append(suffixes[i])
+                
+            words = temp + words
+            i += 1
+        
+        return " ".join(words)
